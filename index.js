55cc7db01221683452cd8dfc894c9db8ebc4582e
Ali McGarry <ali.mcgarry@gmail.com>
2022-06-08 23:00:21 +0100

    Added post put and delete routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mysql = require('promise-mysql')
 const app = express()
+app.use(express.json())
 const port = 3000
 
 const getDatabase = async () => {
@@ -14,8 +15,21 @@ const getDatabase = async () => {
 
 app.get('/products', async (req, res) => {
     const connection = await getDatabase()
-    const products = await connection.query('SELECT `id`, `image`, `title`, `price` FROM `products`')
+    const category = req.query.category
+    const character = req.query.character
+    let query = 'SELECT `id`, `image`, `title`, `price` FROM `products` WHERE `deleted` != 1'
+    const values = []
+    if (category) {
+        query += ' AND `category` = ?'
+        values.push(category)
+    }
+    if (character) {
+        query += ' AND `character` = ?'
+        values.push(character)
+    }
+    const products = await connection.query(query, values)
     res.json(products)
+    res.status(200)
 })
 
 app.get('/products/:id', async (req, res) => {
@@ -23,6 +37,31 @@ app.get('/products/:id', async (req, res) => {
     const connection = await getDatabase()
     const product = await connection.query('SELECT * FROM `products` WHERE `id` = ?;', [urlId])
     res.json(product)
+    res.status(200)
+})
+
+app.post('/products', async (req, res) => {
+    const connection = await getDatabase()
+    await connection.query('INSERT INTO `products` (`title`, `price`, `image`, `category_id`, `category`, `character_id`, `character`, `description`, `image2`, `image3`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', [
+        req.body.title, req.body.price, req.body.image, req.body.category_id, req.body.category, req.body.character_id, req.body.character, req.body.description, req.body.image2, req.body.image3
+    ])
+    res.sendStatus(200)
+})
+
+app.put('/products/:id', async (req, res) => {
+    const urlId = parseInt(req.params.id)
+    const connection = await getDatabase()
+    await connection.query('UPDATE `products` SET `title` = ?, `price` = ?, `image` = ?, `category_id` = ?, `category` = ?, `character_id` = ?, `character` = ?, `description` = ?, `image2` = ?, `image3` = ? WHERE `id` = ?;', [
+        req.body.title, req.body.price, req.body.image, req.body.category_id, req.body.category, req.body.character_id, req.body.character, req.body.description, req.body.image2, req.body.image3, urlId
+    ])
+    res.sendStatus(200)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const urlId = parseInt(req.params.id)
+    const connection = await getDatabase()
+    await connection.query('UPDATE `products` SET `deleted` = 1 WHERE (`id`) = ?;', [urlId])
+    res.sendStatus(200)
 })
 
 app.listen(port)
